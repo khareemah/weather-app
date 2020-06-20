3c35c969fcfea94e9c2466ae773e1c3e0d403342
khareemah <ajimobikareemah@gmail.com>
2020-06-20 07:07:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function() {
+  let long;
+  let lat;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      console.log(long, lat);
+      let api = `https://api.weatherbit.io/v2.0/current?lat=${lat}lon=${long},NC&key=a05e202da04d4432a742b43238169bcc`;
+      fetch(api)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+        });
+    });
+  }
+});
