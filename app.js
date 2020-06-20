2d537736970cab221be9c46b44e0a2ca657b58ad
khareemah <ajimobikareemah@gmail.com>
2020-06-20 07:34:39 -0700

    changed spell error

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function() {
       long = position.coords.longitude;
       lat = position.coords.latitude;
       console.log(long, lat);
-      let api = `https://api.weatherbit.io/v2.0/current?lat=${lat}lon=${long},NC&key=a05e202da04d4432a742b43238169bcc`;
+      let api = `https://api.weatherbit.io/v2.0/current?lat=${lat}long=${long},NC&key=a05e202da04d4432a742b43238169bcc`;
       fetch(api)
         .then(res => res.json())
         .then(data => {
